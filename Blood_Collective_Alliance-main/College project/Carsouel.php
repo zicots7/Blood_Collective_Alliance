69c9e23eca88e4170e13887a84e914b5f3b8fde1
FlexiDriod <140748150+FlexiDriod@users.noreply.github.com>
2024-05-09 14:57:44 +0530

    Add files via upload (#16)

---
+++ Blood_Collective_Alliance-main/College project/Carsouel.php
@@ -0,0 +1,48 @@
+<!-- <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+    
+  <!-- </head>
+  <body class="p-3 m-0 border-0 bd-example m-0 border-0"> -->
+
+    <!-- Example Code -->
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <?php
+    echo" <div class='bg-dark col  pb-4 pt-3 mt-5 px-md-5'>
+    <p style='color:white' class='text-center fw-bolder'>The Blood Collective Alliance is made to simplify the
+        procedure of blood donation .Our goal is to make sure anybody can donate and request for blood if
+        required.With observing every modern aspects and various approaches, Blood Collective Alliance is a
+        simple to use for everyone.
+        All the following components of the Blood Collective Alliance is reserved copyright is claimed .
+    </p>
+</div>";
+      echo'<div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#555" dy=".3em">First slide</text></svg>
+        </div>
+        <div class="carousel-item">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Second slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#666"></rect><text x="50%" y="50%" fill="#444" dy=".3em">Second slide</text></svg>
+        </div>
+        <div class="carousel-item">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Third slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#555"></rect><text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text></svg>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>';
+    ?>
+  <!-- </body>
+</html> -->
---
+++ Blood_Collective_Alliance-main/College project/_DB.php
@@ -0,0 +1,22 @@
+    <?php
+    try{
+        $host = 'localhost';
+        $dbname = 'Blood_Collective_Allaiance_DB';
+        $user = 'postgres';
+        $password = 'Silver@123';
+
+        $db_connect = pg_connect("host=$host dbname=$dbname user=$user password=$password");
+
+        if (!$db_connect) {
+            echo "Failed to connect to PostgreSQL database.";
+            exit;
+        }
+        // } else {
+        //     echo "Connected to PostgreSQL successfully!";
+        // }
+    }
+    catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+        // pg_close($db_connect);
+    ?>
