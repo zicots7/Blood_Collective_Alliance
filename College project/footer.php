a79cd356cde3e12ff26d2917c57253cd96f6de77
Zico.T.S <54213045+zicots7@users.noreply.github.com>
2024-05-17 19:40:48 +0530

    Revert "Add files via upload (#18)"
    This reverts commit 6090508cafd8826007b1eb79ccd1af532abf26d6.

--- College project/footer.php
+++ College project/footer.php
@@ -1,27 +1,22 @@
-<link href="footer.css" rel="stylesheet">
+
 <?php
 echo'
-<footer class="main-footer">
-        <div class="footer-lower">
-          <div class="container-fluid">
-            <div class="row text-center">
-              <div class="col-md-6 col-sm-12 col-xs-12 footer-logo text-center "><span id="copyYear">© 2023 -2024</span> <a href="#" target="_blank">by Blood Colkective Allaince</a></div>
-              <div class="col-md-6 col-sm-12 col-xs-12 footer-logo text-center "> ®&nbsp;Designed and Developed by<a href="#" target="_blank"> BCA Dept. of Burdwan Raj College</a> </div>
-            </div>
-            <div class="col-md-12 col-sm-12 col-xs-12 footer-logo text-center ">
-              <span><a id="mylink" href="#">Terms &amp; Conditions</a></span> |
-              <span><a id="mylink" href="#">Privacy Policy</a></span> | <span><a id="mylink" href="#">Accessibility Statement</a></span> |
-              <span id="updatetime">Last Updated : May 16 2024</span> | 
-            </div>
-            <script>
-              document.getElementById("copyYear").innerHTML = "&copy; 2023 -" + new Date().getFullYear();
-              var d = (new Date()).toString().split(" ").splice(1, 3).join(" ");
-              document.getElementById("updatetime").innerHTML = "Last Updated : " + d;
-            </script>
+<link rel="stylesheet" href="footer.css">
+<div id ="footer-font" class= "container-fluid fixed-bottom " style= "background-color: #fcacba">
+<footer style= "background-color: #fcacba;border-top-style: solid;border-top-width: 0px;padding-right: 0px;padding-left: 0px;" class= "container-fluid=""  d-flex="" flex-wrap="" justify="" align-items-center="" py-3="" my-4="" border-top="" " >
+  <div class="col-md-4 d-flex align-items-center">
+    <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+      <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+    </a>
+    <span class="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+  </div>
 
-          </div>
-        </div>
-      </footer>
+  <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+    <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+    <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+    <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+  </ul>
+</footer>
+</div>
 ';
 ?>
-
